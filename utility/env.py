b1b7540533cde4bcaa4923198573cef32f4d37fc
drinks <drinksober@foxmail.com>
2017-09-15 18:51:12 +0800

    add: add module

--- utility/env.py
+++ utility/env.py
@@ -4,14 +4,15 @@
 def replace(old='.env', new='.newenv'):
     try:
         with open('.newenv', 'r') as new:
-            new = (x.split('=') for x in new.read().splitlines())
+            new = [x.split('=', 1) for x in new.read().splitlines() if x]
             new = {x[0]: x[1] for x in new}
-    except:
+    except Exception as e:
+        print(e)
         new = {}
     lines = []
     with open('.env', 'r') as old:
         for line in old.read().splitlines():
-            line = line.split('=')
+            line = line.split('=', 1)
             if not len(line) == 2 and isinstance(line, list):
                 lines.append('='.join(line))
             else:
@@ -19,8 +20,8 @@ def replace(old='.env', new='.newenv'):
                 lines.append('{0}={1}'.format(line[0], value))
     with open('.env', 'w') as env:
         env.write('\n'.join(lines))
-        import os
-        try:
-            os.unlink('.newenv')
-        except:
-            pass
+    import os
+    try:
+        os.unlink('.newenv')
+    except:
+        pass
