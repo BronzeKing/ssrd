0949fe9db281b39a03ed39a8eb4691ef11398d11
mum5 <mm@mum5.cn>
2017-12-06 10:22:43 +0800

    add: 增加token验证

--- ssrd/accounts/urls.py
+++ ssrd/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
 from .views import LoginView, PasswordResetView, PasswordChangeView, CredentialView, RegisterView, CaptchaView
+from rest_framework_jwt.views import VerifyJSONWebToken
 
 urlpatterns = [
     url(r'^login/$', LoginView.as_view(), name='account_login'),
+    url(r'^token/verify$', VerifyJSONWebToken.as_view()),
     url(r'^register', RegisterView.as_view(), name='account_signup'),
     url(r'^credential$', CredentialView.as_view()),
     url(r'^captcha$', CaptchaView.as_view()),
